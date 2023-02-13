c7c36e6f9e3003ff318306a7ded26fd2b1df2269
oscar <echezuriaoscar@gmail.com>
2023-02-12 22:01:08 -0400

    se modifico

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,8 @@ const { Router } = require("express");
 const { db } = require("../firebase");
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const querySnapshot = await db.collection("contacts").get();
-
-  const contacts = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-
-  console.log(contacts);
-
-  res.send("Bienvenido a mi pagina ayeendo");
+router.get("/", (req, res) => {
+  res.send("hola bienvenido ayeendo");
 });
 
 module.exports = router;
