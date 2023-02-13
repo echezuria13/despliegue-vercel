9842f91b52c812b9fbc699003dd2be68667f2967
oscar <echezuriaoscar@gmail.com>
2023-02-12 22:14:28 -0400

    prueba 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,17 @@ const { Router } = require("express");
 const { db } = require("../firebase");
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("hola bienvenido ayeendo");
+router.get("/", async (req, res) => {
+  const querySnapshot = await db.collection("contacts").get();
+
+  const contacts = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  console.log(contacts);
+
+  res.send("Bienvenido a mi pagina ayeendo");
 });
 
 module.exports = router;
